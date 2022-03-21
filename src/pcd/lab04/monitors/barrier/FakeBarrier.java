c7b456b7009723c0c5ab7837b8b5dd8a39fbe16f
BrunoEsposito2 <bruno.esposito2@studio.unibo.it>
2022-03-21 14:34:49 +0100

    Exercise 1 lab4 (all variants)

--- src/pcd/lab04/monitors/barrier/FakeBarrier.java
+++ src/pcd/lab04/monitors/barrier/FakeBarrier.java
@@ -6,13 +6,29 @@ package pcd.lab04.monitors.barrier;
 public class FakeBarrier implements Barrier {
 
 	private int nParticipants;
+	private int nHits;
 	
 	public FakeBarrier(int nParticipants) {
 		this.nParticipants = nParticipants;
+		this.nHits = 0;
 	}
 	
 	@Override
-	public void hitAndWaitAll() throws InterruptedException {	
+	public synchronized void hitAndWaitAll() throws InterruptedException {
+		this.nHits++;
+		while (this.nHits < this.nParticipants) {
+			wait();
+		}
+		notifyAll(); //better than notify only -> is more foolproof, but a bit less efficient
+		/*this.nHits++;
+		if (this.nHits == this.nParticipants)
+			notifyAll();
+		else 
+			/*
+			 * while instead of only wait because of spurious signal/wake ups made by JVM/Java specific language
+			 * */
+			/*while (this.nHits < this.nParticipants) 
+				wait();*/
 	}
 
 	
